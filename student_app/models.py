5e63dc623f2a3804c2d3639b4f8e82ef4d25adf6
shanmugakalap <shanmugakala.p@kaditinnovations.com>
2024-10-24 08:52:25 +0530

    DOB field added

--- student_app/models.py
+++ student_app/models.py
@@ -5,6 +5,7 @@ class studentmodel(models.Model):
     dept = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     phonenumber = models.CharField(max_length=15)
+    DOB = models.DateField(null=True, blank=True)
 
 def __str__(self):
     return self.name
